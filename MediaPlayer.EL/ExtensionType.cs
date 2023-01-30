4cc50ac1da32821e7894b2079ee6f14a44a62e9b
Димитър Янакиев <dyanakiev@codingburgas.bg>
2023-01-30 12:06:50 +0200

    Initial commit

---
+++ MediaPlayer.EL/ExtensionType.cs
@@ -0,0 +1,11 @@
+﻿namespace MediaPlayer.EL
+{
+    public enum ExtensionType
+    {
+        jpg,
+        png,
+        mp4,
+        Wav,
+        wrong
+    }
+}
---
+++ MediaPlayer.EL/IMediaFile.cs
@@ -0,0 +1,17 @@
+﻿namespace MediaPlayer.EL
+{
+    public interface IMediaFile
+    {
+        int ID { get; set; }
+
+        string Name { get; set; }
+
+        ExtensionType Extension { get; set; }
+        string Description { get; set; }
+
+        int Time_Interval { get; set; }
+
+        string Source { get; set; }
+
+    }
+}
